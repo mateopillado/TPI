fbe865d2e0ee029780e0decddbc2e7af8208862e
Nicolas <nicolas.mauricio.chiofalo@gmail.com>
2024-10-27 16:53:34 -0300

    add changes of history page

--- Front/ClaseJs/pages/Historial/historial.js
+++ Front/ClaseJs/pages/Historial/historial.js
@@ -7,12 +7,13 @@ function init() {
             routineName: "Rutina de Fuerza 1",
             date: "2024-01-08",
             duration: "1h 30m",
+            cantidadseries:3,
             exercises: [
-                { name: "Bench Press", sets: [{ weight: "60", reps: "6", rpe: "8" }, { weight: "70", reps: "5", rpe: "9" }] },
-                { name: "Front Squat", sets: [{ weight: "80", reps: "4", rpe: "8" }] },
-                { name: "Deadlift", sets: [{ weight: "100", reps: "5", rpe: "8" }] },
-                { name: "Pull Up", sets: [{ weight: "bodyweight", reps: "5", rpe: "7" }] },
-                { name: "Overhead Press", sets: [{ weight: "40", reps: "8", rpe: "6" }] }
+                {cantidadseries:3, name: "Bench Press", sets: [{ weight: "60", reps: "6", rpe: "8" }, { weight: "70", reps: "5", rpe: "9" }] },
+                {cantidadseries:3, name: "Front Squat", sets: [{ weight: "80", reps: "4", rpe: "8" }] },
+                {cantidadseries:3, name: "Deadlift", sets: [{ weight: "100", reps: "5", rpe: "8" }] },
+                {cantidadseries:3, name: "Pull Up", sets: [{ weight: "bodyweight", reps: "5", rpe: "7" }] },
+                {cantidadseries:3, name: "Overhead Press", sets: [{ weight: "40", reps: "8", rpe: "6" }] }
             ]
         },
         {
@@ -21,11 +22,11 @@ function init() {
             date: "2024-01-15",
             duration: "1h 15m",
             exercises: [
-                { name: "Deadlift", sets: [{ weight: "120", reps: "3", rpe: "9" }] },
-                { name: "Overhead Press", sets: [{ weight: "50", reps: "6", rpe: "7" }] },
-                { name: "Leg Press", sets: [{ weight: "200", reps: "8", rpe: "8" }] },
-                { name: "Lunges", sets: [{ weight: "40", reps: "10", rpe: "7" }] },
-                { name: "Bench Dips", sets: [{ weight: "bodyweight", reps: "8", rpe: "6" }] }
+                {cantidadseries:3, name: "Deadlift", sets: [{ weight: "120", reps: "3", rpe: "9" }] },
+                {cantidadseries:3, name: "Overhead Press", sets: [{ weight: "50", reps: "6", rpe: "7" }] },
+                {cantidadseries:3, name: "Leg Press", sets: [{ weight: "200", reps: "8", rpe: "8" }] },
+                {cantidadseries:3, name: "Lunges", sets: [{ weight: "40", reps: "10", rpe: "7" }] },
+                {cantidadseries:3, name: "Bench Dips", sets: [{ weight: "bodyweight", reps: "8", rpe: "6" }] }
             ]
         },
         {
@@ -34,11 +35,11 @@ function init() {
             date: "2024-02-20",
             duration: "1h 45m",
             exercises: [
-                { name: "Correr", sets: [{ weight: "0", reps: "30min", rpe: "6" }] },
-                { name: "Ciclismo", sets: [{ weight: "0", reps: "45min", rpe: "7" }] },
-                { name: "Remo", sets: [{ weight: "0", reps: "20min", rpe: "7" }] },
-                { name: "Elíptica", sets: [{ weight: "0", reps: "25min", rpe: "6" }] },
-                { name: "Saltos", sets: [{ weight: "0", reps: "10min", rpe: "8" }] }
+                {cantidadseries:3, name: "Correr", sets: [{ weight: "0", reps: "30min", rpe: "6" }] },
+                {cantidadseries:3, name: "Ciclismo", sets: [{ weight: "0", reps: "45min", rpe: "7" }] },
+                {cantidadseries:3, name: "Remo", sets: [{ weight: "0", reps: "20min", rpe: "7" }] },
+                {cantidadseries:3, name: "Elíptica", sets: [{ weight: "0", reps: "25min", rpe: "6" }] },
+                {cantidadseries:3, name: "Saltos", sets: [{ weight: "0", reps: "10min", rpe: "8" }] }
             ]
         },
         {
@@ -47,11 +48,11 @@ function init() {
             date: "2024-03-01",
             duration: "2h",
             exercises: [
-                { name: "Burpees", sets: [{ weight: "0", reps: "50", rpe: "8" }] },
-                { name: "Kettlebell Swing", sets: [{ weight: "24", reps: "30", rpe: "7" }] },
-                { name: "Box Jump", sets: [{ weight: "0", reps: "20", rpe: "8" }] },
-                { name: "Wall Ball", sets: [{ weight: "9", reps: "25", rpe: "6" }] },
-                { name: "Thruster", sets: [{ weight: "30", reps: "15", rpe: "7" }] }
+                {cantidadseries:2000, name: "Burpees", sets: [{ weight: "0", reps: "50", rpe: "8" }] },
+                {cantidadseries:3, name: "Kettlebell Swing", sets: [{ weight: "24", reps: "30", rpe: "7" }] },
+                {cantidadseries:3, name: "Box Jump", sets: [{ weight: "0", reps: "20", rpe: "8" }] },
+                {cantidadseries:3, name: "Wall Ball", sets: [{ weight: "9", reps: "25", rpe: "6" }] },
+                {cantidadseries:3, name: "Thruster", sets: [{ weight: "30", reps: "15", rpe: "7" }] }
             ]
         }
     ];
@@ -96,27 +97,38 @@ function displaySessions() {
                 </strong> 
                 ${totalWeight} kg
             </p>
-            <h4>Ejercicios y Mejor Serie</h4>
+            <h4 style="display: flex; justify-content: space-between;">
+                Ejercicios
+                <span class="subtitle">Mejor Serie</span>
+            </h4>
             <div class="exercises">
                 ${session.exercises.map(exercise => {
                     const bestSet = findBestSet(exercise.sets);
                     return `
                         <div class="exercise-row">
-                            <p class="exercise-info">
-                                ${exercise.name}: ${exercise.sets.map(set => `${set.reps}x ${set.weight} kg`).join(", ")}
-                            </p>
-                            <span class="best-set">
+                            <p class="exercise-info">${exercise.cantidadseries} x ${exercise.name}: ${exercise.sets.map(set => `${set.reps}x ${set.weight} kg`).join(", ")}</p>
+                            <p class="best-set">
                                 ${bestSet ? `${bestSet.reps} reps, ${bestSet.weight} kg, RPE: ${bestSet.rpe}` : ""}
-                            </span>
+                            </p>
                         </div>
                     `;
                 }).join("")}
             </div>
-            <button onclick="deleteSession(${session.id})">Eliminar</button>
+            <div class="button-group">
+            <button onclick="viewSession(${session.id})">Ver</button>
+                <button onclick="deleteSession(${session.id})">Eliminar</button>
+            </div>
         `;
         sessionList.appendChild(card);
     });
 }
+function viewSession(id) {
+    const session = sessions.find(s => s.id === id);
+    if (session) {
+        alert(`Detalles de la rutina:\nNombre: ${session.routineName}\nFecha: ${session.date}\nDuración: ${session.duration}`);
+    }
+}
+
 
 
 
